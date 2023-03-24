d88833649650a227429e4c7cf250272ca558665e
hasan basri kocagol <91275047+hbasrik@users.noreply.github.com>
2023-03-24 16:52:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+
+const cards = document.querySelectorAll(".card");
+
+let matched = 0;
+let cardOne, cardTwo;
+let disableDeck = false;
+
+var date = new Date().toISOString().slice(0, 10);
+
+console.log(date);
+
+
+var score = 0;
+var scoreDisplay = document.getElementById("score");
+
+function flipCard({target: clickedCard}) 
+{
+    if(cardOne !== clickedCard && !disableDeck) {
+        clickedCard.classList.add("flip");
+
+        if(!cardOne) {
+            return cardOne = clickedCard;
+        }
+        cardTwo = clickedCard;
+        disableDeck = true;
+
+        let cardOneImg = cardOne.querySelector(".back-view img").src,
+        cardTwoImg = cardTwo.querySelector(".back-view img").src;
+        matchCards(cardOneImg, cardTwoImg);
+    }
+}
+
+
+function matchCards(img1, img2) {
+
+    if(img1 === img2) {
+
+        matched++;
+        score++;
+        scoreDisplay.textContent = "Score: " + score;
+        if(matched == 8) {
+            setTimeout(() => {
+                return shuffleCard();
+            }, 1000);
+
+            alert("Congrats, you won !!!");
+            
+        }
+
+        cardOne.removeEventListener("click", flipCard);
+        cardTwo.removeEventListener("click", flipCard);
+
+
+        cardOne = cardTwo = "";
+        return disableDeck = false;
+    }
+
+    setTimeout(() => {
+
+        cardOne.classList.add("shake");
+        cardTwo.classList.add("shake");
+    }, 400);
+
+    setTimeout(() => {
+
+        cardOne.classList.remove("shake", "flip");
+        cardTwo.classList.remove("shake", "flip");
+        cardOne = cardTwo = "";
+
+        disableDeck = false;
+
+    }, 1200);
+}
+
+
+function shuffleCard()
+ {
+    matched = 0;
+    score=0;
+    disableDeck = false;
+
+    cardOne = cardTwo = "";
+
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+
+    arr.sort(() => Math.random() > 0.5 ? 1 : -1);
+
+    cards.forEach((card, i) => {
+
+        card.classList.remove("flip");
+
+        let imgTag = card.querySelector(".back-view img");
+        
+        imgTag.src = `images/${arr[i]}.png`;
+        card.addEventListener("click", flipCard);
+    });
+}
+
+shuffleCard();
+    
+cards.forEach(card => {
+    card.addEventListener("click", flipCard);
+});
